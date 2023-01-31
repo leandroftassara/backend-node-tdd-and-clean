ea25ba66b7e1622f9271cd6189b6f9075bbd5965
leandroftassara <leandroftassara@gmail.com>
2023-01-31 07:11:47 -0300

    refactor: move data layer interfaces to folders

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -8,7 +8,7 @@ import {
   HttpResponse
 } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
-import { LogErrorRepository } from '../../data/protocols/log-error-repository'
+import { LogErrorRepository } from '../../data/protocols/db/log-error-repository'
 import { AccountModel } from '../../domain/models/account'
 
 interface SutTypes {
