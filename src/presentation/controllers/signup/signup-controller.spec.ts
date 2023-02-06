1fe21978071d0befb1182d1a0b33c2ad6d5d82f6
leandroftassara <leandroftassara@gmail.com>
2023-02-06 07:14:32 -0300

    refactor: improve file names

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -1,4 +1,4 @@
-import { SignUpController } from './signup'
+import { SignUpController } from './signup-controller'
 import { MissingParamError, ServerError } from '../../errors'
 import {
   AddAccount,
@@ -6,7 +6,7 @@ import {
   AccountModel,
   HttpRequest,
   Validation
-} from './signup-protocols'
+} from './signup-controller-protocols'
 import {
   successRequest,
   serverError,
